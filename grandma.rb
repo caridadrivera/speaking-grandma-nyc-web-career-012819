d28704a8de1c685f8f67b904bf3e0fad7e5576f7
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-14 19:50:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
     puts "HUH?! SPEAK UP, SONNY!"
   elsif (phrase === phrase.upcase)
     puts "NO, NOT SINCE 1938!"
-  else (phrase ==="I LOVE YOU GRANDMA!")
+  else 
     puts "'I LOVE YOU TOO PUMPKIN!"
   end
 end
