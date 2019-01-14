c9cac4ce2304148c1b3d76829862e915e3f8546d
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-14 20:02:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(speak)
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
-  elsif speak ==
+  elsif speak =="I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   end
 end
